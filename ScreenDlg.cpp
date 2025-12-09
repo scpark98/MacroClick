2530433ed8431608fec506cd9bc7a1ee771cb02c
scpark98 <scpark98@gmail.com>
2025-12-10 08:56:49 +0900

    초기 커밋.

---
+++ ScreenDlg.cpp
@@ -0,0 +1,108 @@
+﻿// ScreenDlg.cpp: 구현 파일
+//
+
+#include "pch.h"
+#include "MacroClick.h"
+#include "afxdialogex.h"
+#include "ScreenDlg.h"
+
+#include "Common/Functions.h"
+#include "Common/MemoryDC.h"
+
+// CScreenDlg 대화 상자
+
+IMPLEMENT_DYNAMIC(CScreenDlg, CDialogEx)
+
+CScreenDlg::CScreenDlg(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_SCREEN, pParent)
+{
+
+}
+
+CScreenDlg::~CScreenDlg()
+{
+}
+
+void CScreenDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+
+BEGIN_MESSAGE_MAP(CScreenDlg, CDialogEx)
+	ON_BN_CLICKED(IDOK, &CScreenDlg::OnBnClickedOk)
+	ON_BN_CLICKED(IDCANCEL, &CScreenDlg::OnBnClickedCancel)
+	ON_WM_ERASEBKGND()
+	ON_WM_PAINT()
+	ON_WM_LBUTTONUP()
+END_MESSAGE_MAP()
+
+
+// CScreenDlg 메시지 처리기
+
+BOOL CScreenDlg::OnInitDialog()
+{
+	CDialogEx::OnInitDialog();
+
+	// TODO:  여기에 추가 초기화 작업을 추가합니다.
+
+	return TRUE;  // return TRUE unless you set the focus to a control
+	// 예외: OCX 속성 페이지는 FALSE를 반환해야 합니다.
+}
+
+void CScreenDlg::OnBnClickedOk()
+{
+	ShowWindow(SW_HIDE);
+}
+
+void CScreenDlg::OnBnClickedCancel()
+{
+	ShowWindow(SW_HIDE);
+}
+
+void CScreenDlg::set_bitmap(CRect r)
+{
+	m_hBitmap = capture_screen_to_bitmap(r);
+	MoveWindow(r);
+	//Invalidate(false);
+	ShowWindow(SW_SHOW);
+
+	m_pt.clear();
+	m_delay.clear();
+}
+BOOL CScreenDlg::OnEraseBkgnd(CDC* pDC)
+{
+	// TODO: 여기에 메시지 처리기 코드를 추가 및/또는 기본값을 호출합니다.
+
+	return CDialogEx::OnEraseBkgnd(pDC);
+}
+
+void CScreenDlg::OnPaint()
+{
+	CPaintDC dc1(this); // device context for painting
+	// TODO: 여기에 메시지 처리기 코드를 추가합니다.
+	// 그리기 메시지에 대해서는 CDialogEx::OnPaint()을(를) 호출하지 마십시오.
+
+	CRect rc;
+	GetClientRect(rc);
+	CMemoryDC dc(&dc1, &rc);
+
+	HDC hdc = CreateCompatibleDC(dc.GetSafeHdc());
+	HBITMAP hBmpOld = (HBITMAP)SelectObject(hdc, m_hBitmap);
+	::BitBlt(dc.GetSafeHdc(), 0, 0, rc.Width(), rc.Height(), hdc, 0, 0, SRCCOPY);
+
+	rc.DeflateRect(300, 300);
+	dc.FillSolidRect(rc, red);
+
+	SelectObject(hdc, hBmpOld);
+}
+
+void CScreenDlg::OnLButtonUp(UINT nFlags, CPoint point)
+{
+	// TODO: 여기에 메시지 처리기 코드를 추가 및/또는 기본값을 호출합니다.
+	m_pt.push_back(point);
+
+	if (m_pt)
+
+	CDialogEx::OnLButtonUp(nFlags, point);
+}
---
+++ ScreenDlg.h
@@ -0,0 +1,39 @@
+﻿#pragma once
+#include "afxdialogex.h"
+
+#include <deque>
+
+// CScreenDlg 대화 상자
+
+class CScreenDlg : public CDialogEx
+{
+	DECLARE_DYNAMIC(CScreenDlg)
+
+public:
+	CScreenDlg(CWnd* pParent = nullptr);   // 표준 생성자입니다.
+	virtual ~CScreenDlg();
+
+	HBITMAP		m_hBitmap;
+	void		set_bitmap(CRect r);
+
+	std::deque<CPoint>	m_pt;
+	std::deque<int>		m_delay;
+	long				m_time_clicked = 0;
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_SCREEN };
+#endif
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 지원입니다.
+
+	DECLARE_MESSAGE_MAP()
+public:
+	virtual BOOL OnInitDialog();
+	afx_msg void OnBnClickedOk();
+	afx_msg void OnBnClickedCancel();
+	afx_msg BOOL OnEraseBkgnd(CDC* pDC);
+	afx_msg void OnPaint();
+	afx_msg void OnLButtonUp(UINT nFlags, CPoint point);
+};
